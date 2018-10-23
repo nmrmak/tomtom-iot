55d85d0bd517a0d71734bb6bc5dde278d30549a5
Nebojsa Mrmak <nebojsa.mrmak@tomtom.com>
2018-10-23 15:08:37 +0200

    Display initialization enabled

--- src/hackathon/hackathon.ino
+++ src/hackathon/hackathon.ino
@@ -1,9 +1,18 @@
-void setup() {
-  // put your setup code here, to run once:
+#include "oled_display.h"
 
+void Initialize() {
+  SSD1306Wire display(I2C_DISPLAY_ADDRESS, SDA_PIN, SCL_PIN);
+  Serial.begin(115200);
+  Serial.println();
+  Serial.print("Initializing display... ");
+  initialize_display(display);
+  Serial.println("done.");  
+}
+
+void setup() {
+  Initialize();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
 }
